51321ea9a3f7a7c7c28ad7f12d00e70fdc178e1b
NBolade-224 <Nickbolade1@gmail.com>
2023-12-17 05:15:28 +0000

    main commit

---
+++ assets/index.js
@@ -0,0 +1,147 @@
+// Global Variants
+const base_url = window.location.origin;
+var processedImages = new Set();
+var cat = 'Football';
+
+// Update Score
+var totalCorrect = 0;
+var totalAnswered = 0;
+function updateScore(result) {
+  totalAnswered = totalAnswered + 1;
+  if (result == true) {totalCorrect = totalCorrect + 1};
+  document.getElementById('ScoreBoxV2').innerHTML = `${totalCorrect}/${totalAnswered}`;
+};
+
+// Reset Score
+function resetScore() {
+  totalCorrect = 0;
+  totalAnswered = 0;
+  document.getElementById('ScoreBoxV2').innerHTML = `${totalCorrect}/${totalAnswered}`;
+  processedImages = new Set();
+  fetchBadge();
+}
+
+// Submit Answer
+var correctAnswer = ""
+async function submitAnswer(number) {
+  const text1 = "answer";
+  var userAnswer = document.getElementById(text1.concat(number)).innerHTML;
+  if (userAnswer == correctAnswer) {
+    console.log('Answer was correct');
+    document.getElementById(text1.concat(number)).style.backgroundColor = "green";
+    updateScore(true);
+  } else {
+    document.getElementById(text1.concat(number)).style.backgroundColor = "red";
+    console.log('Answer was incorrect');
+    updateScore(false);
+  }
+  for (const x of document.getElementsByClassName("answer")) {
+    if (x.innerHTML == correctAnswer) {x.style.backgroundColor = "green";}
+    x.disabled = true;
+  }
+;
+  await new Promise(resolve => setTimeout(resolve, 1000)); // 1 sec
+  fetchBadge();
+}
+
+// Async Fetch (awaits before continuing)
+async function fetchBadge() {
+  // Get filter Selectors for next Bad
+  var group = document.getElementById("teamSelect").value
+  // Reset Color and Disable Status of Answer Boxes
+  for (const x of document.getElementsByClassName("answer")) {
+    x.style.backgroundColor = "";
+    x.disabled = false;
+  }
+  // Fetch new Image from API
+  var response = await fetch(`${base_url}/badge?rand=${Math.random()}&group=${group}&processed=${Array.from(processedImages)}&cat=${cat}&cornm=123`);
+  var [imgSrc,randlist,answer] = await response.json();
+  correctAnswer = answer;
+  // Assign Badge Image and Answers to HTML Tags
+  document.getElementById("badgeImage").src = imgSrc;
+  document.getElementById("answer1").innerHTML = randlist[0][0];
+  document.getElementById("answer2").innerHTML = randlist[1][0];
+  document.getElementById("answer3").innerHTML = randlist[2][0];
+  document.getElementById("answer4").innerHTML = randlist[3][0];
+  // add Answer to processed Answers
+  if (processedImages.has(answer) == true) {
+    processedImages = new Set()
+  };
+  processedImages.add(answer);
+}
+
+// Add Event listeners to each Navbar button to highlight active page
+for (const eachbutton of document.getElementsByClassName("TitleBarButtons")) {
+  eachbutton.addEventListener("click",  function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    cat = this.textContent;
+    fetchBadge();
+  });
+}
+
+// Listener to Resize all elements
+document.getElementById("teamSelect").addEventListener("change", 
+fetchBadge
+);
+window.addEventListener("DOMContentLoaded", 
+  fetchBadge
+);
+window.addEventListener("DOMContentLoaded", 
+  generateFootballTeams
+);
+// log to confirm JavaScript has fully loaded
+console.log(`Java Script Load Complete`);
+
+// Delete Elements (called by Generator functions below)
+function deleteElements() {
+  for (const optionElement of document.querySelectorAll("#teamSelect option")) {
+    optionElement.remove();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Generete Elements
+function generateFootballTeams() {
+  deleteElements();
+  document.getElementById("badgeImage").style.border = "5px solid transparent";
+  document.getElementById("gamePage").style.backgroundImage = "url('/background?type=footballbackground')";
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"All Teams",'all'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Man City",'mancity'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Arsenal",'arsenal'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Aston Villa",'astonvilla'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Bournemouth",'bournemouth'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Brentford",'brentford'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Brighton",'brighton'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Chelsea",'chelsea'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Crystal Palace",'crystalpalace'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Everton",'everton'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Fulham",'fulham'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Leeds",'leeds'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Man Utd",'manutd'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Notts Forest",'nottsforest'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Southampton",'southampton'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Tottenham",'tottenham'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Westham",'westham'];
+  document.getElementById("teamSelect").appendChild(newElement);
+  [newElement,newElement.text,newElement.value] = [document.createElement("option"),"Wolves",'wolves'];
+  document.getElementById("teamSelect").appendChild(newElement);
+}
---
+++ main.py
@@ -0,0 +1,138 @@
+import os, random, psycopg2, boto3, json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from psycopg2 import pool
+
+session = boto3.session.Session()
+client = session.client(
+    service_name='secretsmanager',
+    region_name="eu-west-1"
+)
+
+get_secret_value_response = client.get_secret_value(SecretId="RedshiftCon")
+secret = get_secret_value_response['SecretString']
+secret_dict = json.loads(secret)
+
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    database="postgres",
+    user=secret_dict['awsRSu'],
+    password=secret_dict['awsRSp'],
+    host=secret_dict['awsRSep'],
+    port='5432'
+)
+
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+## Static Assets
+@app.get("/")
+def returnIndex():
+    return FileResponse('./assets/index.html')
+
+@app.get("/js")
+def returnJsAsset():
+    return FileResponse(f'./assets/index.js')
+
+@app.get("/css")
+def returnCssAsset():
+    return FileResponse(f'./assets/index.css')
+
+## Python Logic
+@app.get("/badge")
+def apiReturn(rand,processed,group,cat):
+    ## Rand needed to prevent cache being used
+
+    ## Parse array and convert into set
+    listOfProcessedPics = processed.split(',')
+    processedSet = set(listOfProcessedPics)
+
+    ## Query Database to get list of options to return to user
+    allOptions = databaseQuery(cat,group)
+    filteredOptionsToSelectFrom = []
+
+    ## Loop through the list and remove options that have already been processed
+    for eachOption in allOptions:
+        if eachOption[0] not in processedSet:
+            filteredOptionsToSelectFrom.append(eachOption)
+            continue
+ 
+    ## If there isnt enough pictures remaining to choose from, then make selection from all pictures
+    if len(filteredOptionsToSelectFrom) > 0:
+        options = filteredOptionsToSelectFrom
+    else:
+        options = allOptions
+
+    ## get badge (image to show)
+    correctAnswer = random.choice(options)
+    print(correctAnswer[0])
+
+    ## Remove badge from list (this is where we will select 3 other options to choose from)
+    allOptions.remove(correctAnswer)
+
+    ## Choose three random answers to provide as wrong options
+    listOfOptions = random.sample(allOptions,3)
+
+    ## Add the actual answer onto this list
+    listOfOptions.append(correctAnswer)
+
+    ## Randomise the final list again (to shakeup the order so the correct answer isnt always last)
+    finalOutputList = random.sample(listOfOptions,4)
+
+    ## Return ImageUrl, Options and Answer in request
+    return (f'./image?img={correctAnswer[1]}',finalOutputList,correctAnswer[0])
+
+@app.get("/image")
+def imagereturn(img):
+    try:
+        return FileResponse(f'{img}.jpg')
+    except:
+        return False
+
+@app.get("/background")
+def background(type):
+    return FileResponse(f'./assets/backgrounds/{type}.jpg')
+
+## Database Query Function
+def databaseQuery(category,teamgroup):
+    ## Retrieve Connection From Pool
+    connection = connection_pool.getconn()
+    cursor = connection.cursor()
+
+    if teamgroup == 'all':
+        cursor.execute("""
+            SELECT name, filepath
+            FROM 
+                quizgame
+            WHERE 
+                category = (%s);
+        """, (category,))
+    else:
+        cursor.execute("""
+            SELECT name, filepath 
+            FROM 
+                quizgame
+            WHERE 
+                category = (%s)
+                AND groupteam = (%s);
+        """, (category,teamgroup,))
+
+    data = cursor.fetchall()
+    
+    ## Return Connection To Pool
+    cursor.close()
+    connection_pool.putconn(connection)
+
+    return data
+
+# connection_pool.closeall()
+
